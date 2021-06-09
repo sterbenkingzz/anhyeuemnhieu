66a79b21db87c07bd0095aea39f36559b869c8c3
sterbenkingzz <84759999+sterbenkingzz@users.noreply.github.com>
2021-06-09 14:25:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var min = document.querySelector('.min');
   var second = document.querySelector('.second');
 
-  var startDate = new Date(2021, 4, 25);
+  var startDate = new Date(2007, 8, 13);
   days.innerText = Math.floor((new Date - startDate)/86400000);
   countTime();
 
